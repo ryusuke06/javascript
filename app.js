04bda0904fcf7377992297d3a90320c3d3d6fb19
ryusuke06 <or.06.012@gmail.com>
2020-04-24 22:52:43 +0900

    [finish]9-2

---
+++ app.js
@@ -0,0 +1,15 @@
+var hello = "Hello World";
+alert(hello);
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+	alert("みかんの値段がリンゴより安い")
+}
+else if(orange == apple){
+	alert("みかんとリンゴが同じ値段")
+}
+else{
+	alert("みかんの値段がリンゴより安い")
+}
